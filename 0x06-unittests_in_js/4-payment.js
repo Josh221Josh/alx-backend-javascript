2432bd3724e788707a431cad5a5785ba0b6cf4b4
Josh221Josh <teyilliongroup006@gmail.com>
2025-01-24 08:08:41 +0000

    4-payment.js

--- 0x06-unittests_in_js/4-payment.js
+++ 0x06-unittests_in_js/4-payment.js
@@ -4,5 +4,4 @@ const sendPaymentRequestToApi = (totalAmount, totalShipping) => {
   const totalCost = Utils.calculateNumber('SUM', totalAmount, totalShipping);
   console.log(`The total is: ${totalCost}`);
 };
-
 module.exports = sendPaymentRequestToApi;
