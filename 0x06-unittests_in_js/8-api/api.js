363dd1cee85357fcb5a916ac5b888edcdc4da5a2
Josh221Josh <teyilliongroup006@gmail.com>
2025-01-24 08:18:16 +0000

    8-api

--- 0x06-unittests_in_js/8-api/api.js
+++ 0x06-unittests_in_js/8-api/api.js
@@ -1,11 +1,14 @@
 const express = require('express');
+
 const app = express();
-const port = 7865;
+const PORT = 7865;
 
-app.get('/', (req, res) => {
-  res.end('Welcome to the payment system');
+app.get('/', (_, res) => {
+  res.send('Welcome to the payment system');
 });
 
-app.listen(port, () => {
-  console.log('API available on localhost port 7865');
+app.listen(PORT, () => {
+  console.log(`API available on localhost port ${PORT}`);
 });
+
+module.exports = app;
